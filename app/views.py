7f5dd91fb52de57cd96d3bec85bb54a90d509980
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2022-09-19 08:22:16 -0300

    05.04_Criando_Novas_Views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+
+def horario_atual(request):
+    horario_atual = datetime.datetime.now()
+    return HttpResponse(horario_atual)
