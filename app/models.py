33628e2f550c7f7e3f86cdfa8606dd1303c7eca3
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2022-09-21 08:22:10 -0300

    08.03_Criando_Primeiro_Model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    idade = models.IntegerField(null=False, blank=True)
+    data_nascimento = models.DateField(null=False, blank=False)
+    is_ativo = models.BooleanField(null=False, default=True)
